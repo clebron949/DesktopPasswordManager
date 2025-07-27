6c641989bbbf1b02f001d05aeece80abfe271231
Chris Lebron <christopher.lebron1@gmail.com>
2025-07-27 07:45:56 -0400

    refactor: add more dark mode changes

--- src/renderer/components/Tooltip.vue
+++ src/renderer/components/Tooltip.vue
@@ -1,6 +1,5 @@
 <!-- Tooltip.vue -->
 <script setup lang="ts">
-
 interface Props {
   text: string;
 }
@@ -13,12 +12,12 @@ defineProps<Props>();
     <!-- This is where the trigger element (icon, button, etc.) will be rendered -->
     <slot />
     <div
-      class="pointer-events-none absolute bottom-full left-1/2 z-10 mb-2 -translate-x-1/2 whitespace-nowrap rounded-md bg-slate-800 px-3 py-1.5 text-xs font-semibold text-white opacity-0 transition-opacity duration-200 group-hover:opacity-100"
+      class="pointer-events-none absolute bottom-full left-1/2 z-10 mb-2 -translate-x-1/2 whitespace-nowrap rounded-md bg-slate-800 dark:bg-slate-900 px-3 py-1.5 text-xs font-semibold text-white dark:text-slate-50 opacity-0 transition-opacity duration-200 group-hover:opacity-100"
     >
       {{ text }}
       <!-- The Arrow -->
       <div
-        class="absolute left-1/2 top-full h-0 w-0 -translate-x-1/2 border-x-8 border-t-8 border-x-transparent border-t-slate-800"
+        class="absolute left-1/2 top-full h-0 w-0 -translate-x-1/2 border-x-8 border-t-8 border-x-transparent border-t-slate-800 dark:border-t-gray-900"
       ></div>
     </div>
   </div>
